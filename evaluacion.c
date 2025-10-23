8e21aaeb451d7040a7a3ff9f69dc4effff87a32c
YeralCreaciones <yeralcreaciones497@gmail.com>
2025-10-23 15:58:33 -0300

    Pequeño error tablero

--- evaluacion.c
+++ evaluacion.c
@@ -10,28 +10,59 @@ Tema:
 #define COLUMNAS 7
 
 /*Prototipos*/
-void leertablero(char [][COLUMNAS]);
+int leertablero(char [][COLUMNAS]);
 void mostrar_tab(char [][COLUMNAS]);
+void ajuste_tablero(char[][COLUMNAS], int);
+void borrar_fila(char [][COLUMNAS], int);
 
 /*Funcion principal*/
 int main(){
     char tablero[FILAS][COLUMNAS];
-    leertablero(tablero);
+    int filas;
+    filas = leertablero(tablero);
+    mostrar_tab(tablero);
     return 0;
 }
 
 /*Funciones*/
-void leertablero(char tab[FILAS][COLUMNAS]){
+int leertablero(char tab[FILAS][COLUMNAS]){
     int i = 0;
-    for ( i = 0; i < FILAS; i++){
+    while (i < FILAS){
         scanf("%s", tab[i]);
-        printf("%c\n", tab[i][1]);
+        i++;
     }
+    i--;
+    ajuste_tablero(tab, i);
+    return i;
 }
 
 void mostrar_tab(char tab[FILAS][COLUMNAS]){
     int i, j;
     for(i = 0; i < FILAS; i++){
-        printf("%s", tab[i]);
+        for ( j = 0; j < COLUMNAS; j++){
+            printf("%c", tab[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void ajuste_tablero(char tab[FILAS][COLUMNAS], int filas){
+    int i, j, centro, contador;
+    char espacio[FILAS];
+    char proceso[FILAS][COLUMNAS];
+    espacio[0] = ' ';
+    centro = FILAS/ 2;
+    strcpy(tab[centro], espacio);
+    strcpy(proceso[0], tab[centro+1]);
+    borrar_fila(tab, centro);
+    strcpy(tab[centro], proceso[0]);
+}
+
+void borrar_fila(char tab[FILAS][COLUMNAS], int centro){
+    int i, j;
+    for (i = centro + 1; i < FILAS; i++){
+        for ( j = 0; j < COLUMNAS; j++){
+            tab[i][j] = '\0';
+        }
     }
 }
