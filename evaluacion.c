c51c9508a4c238f0820bca6b9712b39744b00942
YeralCreaciones <yeralcreaciones497@gmail.com>
2025-10-23 18:13:53 -0300

    Tablero arreglado

--- evaluacion.c
+++ evaluacion.c
@@ -6,11 +6,11 @@ Tema:
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define FILAS 8
+#define FILAS 7
 #define COLUMNAS 7
 
 /*Prototipos*/
-int leertablero(char [][COLUMNAS]);
+void leertablero(char [][COLUMNAS]);
 void mostrar_tab(char [][COLUMNAS]);
 void ajuste_tablero(char[][COLUMNAS], int);
 void borrar_fila(char [][COLUMNAS], int);
@@ -18,22 +18,21 @@ void borrar_fila(char [][COLUMNAS], int);
 /*Funcion principal*/
 int main(){
     char tablero[FILAS][COLUMNAS];
-    int filas;
-    filas = leertablero(tablero);
+    leertablero(tablero);
     mostrar_tab(tablero);
     return 0;
 }
 
 /*Funciones*/
-int leertablero(char tab[FILAS][COLUMNAS]){
-    int i = 0;
-    while (i < FILAS){
-        scanf("%s", tab[i]);
-        i++;
+void leertablero(char tab[FILAS][COLUMNAS]){
+    int i, j;
+    char salto;
+    for (i = 0; i < 7; i++) {
+        for (j = 0; j < 7; j++) {
+            scanf("%c", &tab[i][j]);
+        }
+        scanf("%c", &salto);
     }
-    i--;
-    ajuste_tablero(tab, i);
-    return i;
 }
 
 void mostrar_tab(char tab[FILAS][COLUMNAS]){
@@ -43,26 +42,5 @@ void mostrar_tab(char tab[FILAS][COLUMNAS]){
             printf("%c", tab[i][j]);
         }
         printf("\n");
-    }
-}
-
-void ajuste_tablero(char tab[FILAS][COLUMNAS], int filas){
-    int i, j, centro, contador;
-    char espacio[FILAS];
-    char proceso[FILAS][COLUMNAS];
-    espacio[0] = ' ';
-    centro = FILAS/ 2;
-    strcpy(tab[centro], espacio);
-    strcpy(proceso[0], tab[centro+1]);
-    borrar_fila(tab, centro);
-    strcpy(tab[centro], proceso[0]);
-}
-
-void borrar_fila(char tab[FILAS][COLUMNAS], int centro){
-    int i, j;
-    for (i = centro + 1; i < FILAS; i++){
-        for ( j = 0; j < COLUMNAS; j++){
-            tab[i][j] = '\0';
-        }
     }
 }
